c08db9028514fbb5f2dc2aef9bd2757b64be88b3
biS <hso2@masonlive.gmu.edu>
2020-04-24 22:47:31 -0400

    L-002: Check out to chapter2 - TrackCoach

--- src/com/so/simpledemo/MyApp.java
+++ src/com/so/simpledemo/MyApp.java
@@ -2,7 +2,7 @@ package com.so.simpledemo;
 
 public class MyApp {
 	public static void main(String[]args) {
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		System.out.println(theCoach.getDailyWorkout());
 	}
 }
---
+++ src/com/so/simpledemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.so.simpledemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "this comes from trackcoach";
+	}
+
+}
